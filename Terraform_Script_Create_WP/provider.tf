705350e25fae4518249d991c09869acb2eb53610
Aaron Saikovski <asaikovski@outlook.com>
2022-11-15 19:41:18 +1100

    added Terraform build and fixed Bicep code

---
+++ Terraform_Script_Create_WP/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "local" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.30.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+    }
+  }
+}
+
+provider "azapi" {
+  # More information on the authentication methods supported by
+  # the AzApi Provider can be found here:
+  # https://registry.terraform.io/providers/Azure/azapi/latest/docs
+
+  # subscription_id = "..."
+  # client_id       = "..."
+  # client_secret   = "..."
+  # tenant_id       = "..."
+}
+
+provider "azurerm" {
+  features {}
+}
